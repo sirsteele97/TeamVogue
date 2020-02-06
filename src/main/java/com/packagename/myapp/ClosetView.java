1ff91434fb12d99b6f6fca8ab0a1e060bea350f9
grace-the-ace <mgdavanzo26@gmail.com>
2020-02-06 18:23:39 -0500

    Closetview

--- src/main/java/com/packagename/myapp/ClosetView.java
+++ src/main/java/com/packagename/myapp/ClosetView.java
@@ -4,10 +4,10 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +24,10 @@ import org.springframework.beans.factory.annotation.Autowired;
  * A new instance of this class is created for every new user and every
  * browser tab/window.
  */
-@Route
-@PWA(name = "Vaadin Application",
-        shortName = "Vaadin App",
-        description = "This is an example Vaadin application.",
-        enableInstallPrompt = true)
+@Route(value="closet")
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends VerticalLayout {
+public class ClosetView extends FormLayout {
 
     /**
      * Construct a new Vaadin view.
@@ -40,27 +36,28 @@ public class MainView extends VerticalLayout {
      *
      * @param service The message service. Automatically injected Spring managed bean.
      */
-    public MainView(@Autowired GreetService service) {
+    public ClosetView(@Autowired GreetService service) {
 
-        // Use TextField for standard text input
+
         Label title = new Label("Closet");
 
         // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello");
+        Button logout= new Button("Logout");
+        Button upload= new Button("Upload");
                 //e -> Notification.show(service.greet(textField.getValue()));
 
         // Theme variants give you predefined extra styles for components.
         // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        //lo.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
         // You can specify keyboard shortcuts for buttons.
         // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
+        //button.addClickShortcut(Key.ENTER);
 
         // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
         addClassName("centered-content");
 
-        add(title, button);
+        add(title, logout);
     }
 
 }
