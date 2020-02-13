3a9120ff63577c018d1103e4eb0b0fd10f1fa15e
king-2349 <ajking.71397@gmail.com>
2020-02-13 11:11:40 -0500

    One param filtering and exact query matches

--- src/main/java/com/packagename/myapp/DiscoveryService.java
+++ src/main/java/com/packagename/myapp/DiscoveryService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -28,16 +29,23 @@ public class DiscoveryService {
         Discovery discovery = new Discovery("2019-04-30",authenticator);
         discovery.setServiceUrl(url);
 
-        String filter = "";
+        LinkedList<String> filters = new LinkedList<String>();
         if(!clothesParam.equalsIgnoreCase("")){
-            filter += "ClothModel: "+clothesParam+", ";
+            filters.add("ClothModel::"+clothesParam);
         }
         if(!colorParam.equalsIgnoreCase("")){
-            filter += "ColorModel: "+colorParam;
+            filters.add("ColorModel::"+colorParam);
+        }
+
+        String filter = "";
+        for(int i=0;i<filters.size();i++){
+            filter += filters.get(i);
+            if(i != filters.size()-1)
+                filter +=", ";
         }
 
         QueryOptions queryOptions = new QueryOptions.Builder(clothesEnvironment,clothesCollection)
-                .query("").filter(filter).build();
+                .query(filter).build();
         QueryResponse response = discovery.query(queryOptions).execute().getResult();
 
         List<QueryResult> results = response.getResults();
