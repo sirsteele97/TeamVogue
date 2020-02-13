5e6a4f76534fb335e89fb6afd487f849bcd2e382
grace-the-ace <38846614+grace-the-ace@users.noreply.github.com>
2020-02-13 06:22:49 -0500

    Add files via upload

--- src/main/java/com/packagename/myapp/ClosetBackupView.java
+++ src/main/java/com/packagename/myapp/ClosetBackupView.java
@@ -1,9 +1,7 @@
 package com.packagename.myapp;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -18,29 +16,11 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.InputStreamFactory;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.server.StreamResource;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.Normalizer;
 import java.util.ArrayList;
-import java.util.List;
-
-import Database.DatabaseFunctions;
-
-import javax.imageio.ImageIO;
-import javax.xml.transform.stream.StreamSource;
 
 import static java.lang.System.out;
 /**
@@ -55,37 +35,13 @@ import static java.lang.System.out;
  * A new instance of this class is created for every new user and every
  * browser tab/window.
  */
-@Route(value="closet")
-
+@Route(value="closetbackup")
+@StyleSheet("./styles/closet-text-field-styles.css")
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class ClosetView extends FormLayout {
-
-    /*private StreamResource createStreamResource(BufferedImage bf) {
-        return new StreamResource("photo",()-> {
-
-    }*/
-
-    Image[] loadImages(List<Database.Image> images, int i, ImageUploaderServiceInterface service){
-        Image[] toLoad=new Image[20];
-
-        for(int j=0;j<20&&(j+i)<images.size();j++){
-            Database.Image ime= images.get(i+j);
-            BufferedImage bf=ime.GetBufferedImage();
-            StreamResource sr=new StreamResource("img", ()->{
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                try {
-                    ImageIO.write(bf, "png", bos);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return new ByteArrayInputStream(bos.toByteArray());
-            });
-            sr.setContentType("image/png");
-            toLoad[j]= new Image(sr,"Clothing Obj");
-        }
-
-        return toLoad;
+public class ClosetBackupView extends FormLayout {
+    static Image[] loadImages(){
+        return new Image[10];
     }
     /**
      * Construct a new Vaadin view.
@@ -95,7 +51,7 @@ public class ClosetView extends FormLayout {
      * @param service The message service. Automatically injected Spring managed bean.
      */
 
-    public ClosetView(@Autowired GreetService guest, ImageUploaderServiceInterface service) {
+    public ClosetBackupView(@Autowired GreetService service) {
         //set up filter presets
         ArrayList<String> clothes = new ArrayList<String>();
         clothes.add("long sleeve shirt");
@@ -115,33 +71,27 @@ public class ClosetView extends FormLayout {
         colors.add("white");
         ComboBox colorsSelect = new ComboBox("Color",colors);
         colorsSelect.setAllowCustomValue(false);
-        //header for closer
-        H1 title=new H1("Closet");
-        //Gets the image database
-        DatabaseFunctions.DBImages db=new DatabaseFunctions.DBImages();
-        //turns all photos in database into a list
-        List<Database.Image> imgs=db.SelectImages(1);
-        int loc=0;
 
-       // MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
+        H1 title=new H1("Closet");
+        //title.setHeight("200px");
+        //title.setWidth("100px");
+
         Div pictureArea = new Div();
-        //add all pictures to area here (pictureArea.add())
-        Image[] imgarray=loadImages(imgs,loc,service);
-        for (int i=0; i<imgarray.length; i++){
-            //imgarray[i]=new Image("https://dummyimage.com/600x400/000/fff", "DummyImage");
-            if(imgarray[i]!=null) {
-                pictureArea.add(imgarray[i]);
-            }
+
+        Image[] imgarray=new Image[10];
+
+        for (int i=0; i<10; i++){
+            imgarray[i]=new Image("https://dummyimage.com/600x400/000/fff", "DummyImage");
+            pictureArea.add(imgarray[i]);
         }
-        //create filter button and functionality
+        //to load images
         Button filterButton = new Button("Filter",e -> {
             pictureArea.removeAll();
             String clothesParam = clothesSelect.getValue().toString();
             String colorParam = colorsSelect.getValue().toString();
         });
 
-        //title.setHeight("200px");
-        //title.setWidth("100px");
+        Div mainStuff = new Div();
         //creates hom and upload buttons
         Button home= new Button(new Icon(VaadinIcon.HOME));
         Button upload = new Button(new Icon(VaadinIcon.UPLOAD));
@@ -173,9 +123,8 @@ public class ClosetView extends FormLayout {
         //button.addClickShortcut(Key.ENTER);
 
         // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        //addClassName("centered-content");
+        addClassName("centered-content");
 
     }
 
 }
-
