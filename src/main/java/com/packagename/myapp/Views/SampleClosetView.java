fc7d24847ab1da465bd6b4e707d458fc5fee8af9
king-2349 <ajking.71397@gmail.com>
2020-02-25 09:54:06 -0500

    Closet item wrapping

--- src/main/java/com/packagename/myapp/Views/SampleClosetView.java
+++ src/main/java/com/packagename/myapp/Views/SampleClosetView.java
@@ -3,12 +3,15 @@ package com.packagename.myapp.Views;
 import com.packagename.myapp.Components.TopBar;
 import com.packagename.myapp.Services.Interfaces.IClothesStorage;
 import com.packagename.myapp.Services.Interfaces.IImageStorage;
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.HtmlComponent;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -50,7 +53,8 @@ public class SampleClosetView extends VerticalLayout {
         ComboBox colorsSelect = new ComboBox("Color",colors);
         colorsSelect.setAllowCustomValue(false);
 
-        HorizontalLayout pictureArea = new HorizontalLayout();
+        FlexLayout pictureArea = new FlexLayout();
+        pictureArea.setWrapMode(FlexLayout.WrapMode.WRAP);
         addImagesToDiv("","",pictureArea);
 
         Button filterButton = new Button("Filter",e -> {
@@ -65,25 +69,33 @@ public class SampleClosetView extends VerticalLayout {
         Div mainStuff = new Div();
         mainStuff.add(clothesSelect, colorsSelect, filterButton,uploadButton);
 
+
         add(new TopBar(),mainStuff,pictureArea);
     }
 
-    public void addImagesToDiv(String clothesParam, String colorParam, HorizontalLayout pictureArea){
+    public void addImagesToDiv(String clothesParam, String colorParam, FlexLayout pictureArea){
         Map<String,Map<String,String>> images = clothesStorageService.getClothes(clothesParam,colorParam);
         for(String imageId : images.keySet()){
-            VerticalLayout picture = new VerticalLayout();
-            Image image = new Image(images.get(imageId).get("ImageLink"),"");
-            image.setWidth("300px");
-            image.setHeight("300px");
-            picture.add(image);
-            picture.add(new Text(images.get(imageId).get("ColorModel")+" , "+images.get(imageId).get("ClothModel")));
-            picture.add(new Button("Delete",e->{
-                clothesStorageService.deleteClothing(imageId);
-                imageStorageService.deleteImage(images.get(imageId).get("DeleteKey"));
-            }));
-
-            pictureArea.add(picture);
+            pictureArea.add(createClothingItem(imageId,images.get(imageId)));
         }
+    }
+
+    public Div createClothingItem(String imageId, Map<String,String> imageAttributes){
+        Div closetItem = new Div();
+
+        Image image = new Image(imageAttributes.get("ImageLink"),"");
+        image.setWidth("300px");
+        image.setHeight("300px");
+        closetItem.add(image);
+        closetItem.add(new HtmlComponent("br"));
+        closetItem.add(new Text(imageAttributes.get("ColorModel")+" , "+imageAttributes.get("ClothModel")));
+        closetItem.add(new HtmlComponent("br"));
+        closetItem.add(new Button("Delete",e->{
+            clothesStorageService.deleteClothing(imageId);
+            imageStorageService.deleteImage(imageAttributes.get("DeleteKey"));
+        }));
+
+        return closetItem;
     }
 
 }
