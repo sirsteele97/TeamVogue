3195cb2b19fe2e40d7db6b293e877e16f12b4fe8
alierdaty <58745063+alierdaty@users.noreply.github.com>
2020-02-25 10:13:27 -0500

    Update TopBar.java
    Added new shortcut buttons

--- src/main/java/com/packagename/myapp/Components/TopBar.java
+++ src/main/java/com/packagename/myapp/Components/TopBar.java
@@ -1,4 +1,4 @@
-package com.packagename.myapp.Components;
+package com.packagename.myapp;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -14,11 +14,19 @@ public class TopBar extends Div {
         closetButton.addClickListener(e -> closetButton.getUI().ifPresent(ui->ui.navigate("closet")));
         closetButton.setClassName("topBarButton");
 
+        Button outfitButton = new Button("Outfit Evaluator");
+        closetButton.addClickListener(e -> closetButton.getUI().ifPresent(ui->ui.navigate("outfit")));
+        closetButton.setClassName("topBarButton");
+
+        Button styleButton = new Button("Find Your Style");
+        closetButton.addClickListener(e -> closetButton.getUI().ifPresent(ui->ui.navigate("style")));
+        closetButton.setClassName("topBarButton");
+
         Button signOutButton = new Button("Sign Out");
         signOutButton.addClickListener(e -> signOutButton.getUI().ifPresent(ui->ui.navigate("")));
         signOutButton.setClassName("topBarButton");
 
-        add(closetButton, signOutButton);
+        add(closetButton, outfitButton, styleButton, signOutButton);
     }
 
 }
