da64cf5ee733653b8f729e27dd54f1ef8b8eb48c
alierdaty <58745063+alierdaty@users.noreply.github.com>
2020-02-25 14:27:12 -0500

    Create StyleView.java
    this is the tinder-learning page

---
+++ src/main/java/com/packagename/myapp/Views/StyleView.java
@@ -0,0 +1,34 @@
+package com.packagename.myapp;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Route(value="style")
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class StyleView extends VerticalLayout {
+
+    private DiscoveryService discoveryService;
+    private S3Service s3Service;
+
+    public OutfitView(@Autowired DiscoveryService discoveryService, @Autowired S3Service s3Service) {
+        this.discoveryService = discoveryService;
+        this.s3Service = s3Service;
+
+        Button button = new Button("STYLE");
+
+        add(new TopBar(), button);
+    }
+}
