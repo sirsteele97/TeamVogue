c3b92b5e37ae671d0a04e1579c36054b9f509825
grace-the-ace <38846614+grace-the-ace@users.noreply.github.com>
2020-03-03 17:46:22 -0500

    Add files via upload

---
+++ src/main/java/com/packagename/myapp/neuralNet/NeuronCls.java
@@ -0,0 +1,28 @@
+public class NeuronCls {
+    double[] weights;
+    public NeuronCls(double [] w){
+        weights=w;
+    }
+
+
+    public double sum(double[] inputs){
+        double sum=0.0;
+        for(int i=0;i<inputs.length;i++){
+            sum=sum+weights[i]*inputs[i];
+        }
+        return sum;
+    }
+    public double run(double[] inputs){
+        
+        double sum=sum(inputs);
+        double output=sum;
+
+        return output;
+    }
+    public void update_weights(double[] add_w){
+        for(int i=0;i<weights.length;i++){
+            weights[i]=weights[i]+add_w[1];
+        }
+    }
+
+}
