c07f52723023b19f2e501964d2f2bc5fd921d5c8
king-2349 <ajking.71397@gmail.com>
2020-03-31 17:42:19 -0400

    Visual recognition multiple recognizers handling

--- src/main/java/com/packagename/myapp/Services/IBMClothesClassifier.java
+++ src/main/java/com/packagename/myapp/Services/IBMClothesClassifier.java
@@ -10,7 +10,6 @@ import com.packagename.myapp.Services.Interfaces.IClothesClassifier;
 import com.packagename.myapp.Utils.KeyHolder;
 import org.springframework.stereotype.Service;
 
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,19 +17,27 @@ import java.util.Map;
 @Service
 public class IBMClothesClassifier implements IClothesClassifier {
 
-    private final String key = KeyHolder.getKey("ibm.visualrecognition.apiKey");
-    private final String url = "https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/e8a1af7f-a932-462f-94e6-addcbd5a69da";
+    private final String[] keys = {KeyHolder.getKey("ibm.visualrecognition1.apiKey"),KeyHolder.getKey("ibm.visualrecognition2.apiKey")};
+    private final String[] urls = {"https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/e8a1af7f-a932-462f-94e6-addcbd5a69da",
+            "https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/2310e2f1-02b7-4dcb-83ce-22be74fe3414"};
+    private final String[] models = {"ClothModel_1452037569","ColorModel_1451624520"};
 
     @Override
     public Map<String, String> getClothingAttributes(String imageUrl) {
-        IamAuthenticator authenticator = new IamAuthenticator(key);
+        Map<String,String> attributes = classify(imageUrl,0);
+        //attributes.putAll(classify(imageUrl,1));
+        return attributes;
+    }
+
+    private Map<String,String> classify(String imageUrl, int recognizer){
+        IamAuthenticator authenticator = new IamAuthenticator(keys[recognizer]);
         VisualRecognition visualRecognition = new VisualRecognition("2018-03-19", authenticator);
-        visualRecognition.setServiceUrl(url);
+        visualRecognition.setServiceUrl(urls[recognizer]);
 
         ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
                 .url(imageUrl)
-                .addClassifierId("ClothModel_1452037569")
-                .addClassifierId("ColorModel_1451624520")
+                .addClassifierId(models[recognizer*2])
+                .addClassifierId(models[recognizer*2+1])
                 .threshold(0f)
                 .build();
         ClassifiedImages results = visualRecognition.classify(classifyOptions).execute().getResult();
