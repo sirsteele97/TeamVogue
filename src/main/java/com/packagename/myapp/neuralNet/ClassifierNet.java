e6c2993acfb6004a7382fbf9ffb616fc6e061b8c
alierdaty <58745063+alierdaty@users.noreply.github.com>
2020-04-05 16:53:27 -0400

    Update ClassifierNet.java

--- src/main/java/com/packagename/myapp/neuralNet/ClassifierNet.java
+++ src/main/java/com/packagename/myapp/neuralNet/ClassifierNet.java
@@ -1,17 +1,27 @@
 package com.packagename.myapp.neuralNet;
 
 public class ClassifierNet {
+    public final double LEARNING_RATE = .01;
+
     NeuronCls[] firstlayer=new NeuronCls[3];
     NeuronCls[] secondlayer=new NeuronCls[1];
+
+    //{solid, stripe, dot, plaid, icon}
+    double[] patternWeights = {0.01, 0.01, 0.01, 0.01, 0.01};
+
     public ClassifierNet(){
+        //{red, green, blue, white, black, 0.5}
         double[] GB= {0.01,1.0,1.0,0.01,0.01,0.5};
         double[] GR= {2.0,2.0,0.01,0.01,0.01,0.5};
         double[] WB= {0.01,0.01,0.01,1.0,1.0,0.5};
-        double[] last ={1.0,-1.0,2.0,0.2};
+
+        //last, not sure what its function is
+        double[] last ={1.0,-1.0,2.0,1.0,0.2};
+
         firstlayer[0]=new NeuronCls(GB);
         firstlayer[1]=new NeuronCls(GR);
         firstlayer[2]=new NeuronCls(WB);
-        secondlayer[0]=new NeuronCls(last);
+        secondlayer[0] = new NeuronCls(last);
     }
 
     public double run(double[] colors){
@@ -22,10 +32,18 @@ public class ClassifierNet {
 
         return secondlayer[0].run(afterInput);
     }
-    public String judge(double[] colors){
+    public String judge(double[] colors, double[] patterns){
         String judgement;
-        double match=run(colors);
-        System.out.println(match);
+
+        //get color value
+        double match = run(colors);
+
+        //get pattern value
+        for(int i = 0; i < patterns.length; i++){
+            match += patterns[i] * patternWeights[i];
+        }
+
+        //output depending on model values
         if(match<0){
             judgement="bad";
         }
@@ -37,7 +55,8 @@ public class ClassifierNet {
         }
         return judgement;
     }
-    public void backpropagation(String desired, double[] inputs, double learning_rate){
+    public void backpropagation(String desired, double[][] datapoint, double learning_rate){
+        double[] inputs = datapoint[0];
         if(desired.equals("good")){
             for(int i=0;i<firstlayer.length;i++){
                 if(secondlayer[0].weights[i]>0){
@@ -52,7 +71,7 @@ public class ClassifierNet {
                 }
             }
         }
-        else{
+        else if (desired.equals("bad")){
             for(int i=0;i<firstlayer.length;i++){
                 if(secondlayer[0].weights[i]>0){
                     for(int j=0;j<firstlayer[i].weights.length-1;j++){
@@ -68,5 +87,15 @@ public class ClassifierNet {
 
         }
 
+        double[] patterns = datapoint[1];
+        if(desired.equals("good")) {
+            for(int i=0; i<patterns.length;i++){
+                patternWeights[i] += patterns[i] * learning_rate;
+            }
+        } else if(desired.equals("bad")){
+            for(int i=0; i<patterns.length;i++){
+                patternWeights[i] -= patterns[i] * learning_rate;
+            }
+        }
     }
 }
