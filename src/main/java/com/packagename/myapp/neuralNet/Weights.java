c91c9396910bcd9ddf40970b9901f0b0802c0779
Rajeev Ravi <rrcool@gmail.com>
2020-04-07 17:47:44 -0400

    Created Weights Class and default file

---
+++ src/main/java/com/packagename/myapp/neuralNet/Weights.java
@@ -0,0 +1,11 @@
+package com.packagename.myapp.neuralNet;
+
+// Class for storing the weights needed for the neural network with the defaults in place.
+class Weights {
+    double[] GB = {0.01,1.0,1.0,0.01,0.01,0.5};
+    double[] GR = {2.0,2.0,0.01,0.01,0.01,0.5};
+    double[] WB = {0.01,0.01,0.01,1.0,1.0,0.5};
+    double[] last = {1.0,-1.0,2.0,1.0,0.2};
+    double[] patternWeights = {0.01, 0.01, 0.01, 0.01, 0.01};
+
+}
